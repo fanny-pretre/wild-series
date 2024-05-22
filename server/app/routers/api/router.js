2eac1e3e9af8e4a59dc81c64a1c6d3b877ebc85b
FANNY PRETRE <fanny.pretre@gmail.com>
2024-05-22 15:32:56 +0200

    New route for programs

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -2,6 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
+const { sayWelcome } = require("../../controllers/sayActions");
+
+router.get("/", sayWelcome);
+
+/*
+const { browse } = require("../../controllers/programActions");
+
+router.get("/", browse);
+*/
+
 /* ************************************************************************* */
 // Import And Use Routers Here
 /* ************************************************************************* */
@@ -9,6 +19,10 @@ const router = express.Router();
 const itemsRouter = require("./items/router");
 
 router.use("/items", itemsRouter);
+
+const programsRouter = require("./programs/router");
+
+router.use("/programs", programsRouter);
 
 /* ************************************************************************* */
 
